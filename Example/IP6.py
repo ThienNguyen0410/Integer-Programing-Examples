24e79c4bfe04789ce506286f5d94c1fc769609ef
Huu Thien <Huu Thien>
2025-10-13 08:01:23 +0700

    Fix IP6

--- Example/IP6.py
+++ Example/IP6.py
@@ -1,31 +1,33 @@
 from ortools.linear_solver import pywraplp
+def IP_example6():
+    solver = pywraplp.Solver.CreateSolver('SCIP')
 
-solver = pywraplp.Solver.CreateSolver('SCIP')
+    #Declare variables
+    x = {}
+    for i in range (1,4):
+        x[i] = solver.IntVar(0,solver.infinity(),f'x{i}')
 
-#Declare variables
-x = {}
-for i in range (1,4):
-    x[i] = solver.IntVar(0,solver.infinity(),f'x{i}')
+    y = {}
+    for j in range (1,4):
+        y[j] = solver.BoolVar(f'x{j}')
 
-y = {}
-for j in range (1,4):
-    y[j] = solver.BoolVar(f'x{j}')
-
-# Add constraints
-for i in range(1,4):
-    solver.Add(x[i] <= 2000*y[i])
-    solver.Add(1000 - x[i] <= 2000*(1-y[i]))
-solver.Add(1.5*x[1] + 3*x[2] + 5*x[3] <= 6000)
-solver.Add(30*x[1] + 25*x[2] + 40*x[3] <= 60000)
-solver.Maximize(2*x[1] + 3*x[2] + 4*x[3])
-
-status = solver.Solve()
-if status == pywraplp.Solver.OPTIMAL:
-    print("Result of example 6:")
-    print(f"Optimal value z   = {solver.Objective().Value()}")
+    # Add constraints
     for i in range(1,4):
-        print(f"{x[i].name()} = {x[i].solution_value()}")
-    
-    for i in range(1,4):
-        print(f"{y[i].name()} = {y[i].solution_value()}")
-    
+        solver.Add(x[i] <= 2000*y[i])
+        solver.Add(1000 - x[i] <= 2000*(1-y[i]))
+    solver.Add(1.5*x[1] + 3*x[2] + 5*x[3] <= 6000)
+    solver.Add(30*x[1] + 25*x[2] + 40*x[3] <= 60000)
+    solver.Maximize(2*x[1] + 3*x[2] + 4*x[3])
+
+    status = solver.Solve()
+    if status == pywraplp.Solver.OPTIMAL:
+        print("Result of example 6:")
+        print(f"Optimal value z   = {solver.Objective().Value()}")
+        for i in range(1,4):
+            print(f"{x[i].name()} = {x[i].solution_value()}")
+        
+        for i in range(1,4):
+            print(f"{y[i].name()} = {y[i].solution_value()}")
+        
+if __name__ == "__main__":
+    IP_example6()
